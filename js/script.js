5ec2e3eb5212c8c0df61d165184b2cede56f41b2
semhar <semaria.eyzac@gmal.com>
2018-02-02 22:30:59 -0800

    creat html, css and js folders and files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var add = function(number1, number2){
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter another number"));
+
+alert(add(number1, number2));
+
+var subtract = function(number1, number2){
+return number1 - number2;
+};
+
+var number1 = parseInt(prompt("enter a number"));
+var number2 = parseInt(prompt("enter another number"));
+
+alert(subtract(number1, number2));
