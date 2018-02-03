6516c6725e2bdac114927a582ff65eba63536fc1
semhar <semaria.eyzac@gmal.com>
2018-02-02 22:34:36 -0800

    add script link to html file

--- js/script.js
+++ js/script.js
@@ -6,12 +6,3 @@ var number1 = parseInt(prompt("Enter a number"));
 var number2 = parseInt(prompt("Enter another number"));
 
 alert(add(number1, number2));
-
-var subtract = function(number1, number2){
-return number1 - number2;
-};
-
-var number1 = parseInt(prompt("enter a number"));
-var number2 = parseInt(prompt("enter another number"));
-
-alert(subtract(number1, number2));
