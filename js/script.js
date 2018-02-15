4fba269faf2ea4284e49d19863083ba49bbf23d4
Guest User <Guest@Epicodus-8E.local>
2018-02-14 20:13:18 -0800

    add nw features of js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,48 @@
+//Biz Logic
 var add = function(number1, number2){
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+//user Logic
+$(document).ready(function(){
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $("#result").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mul1").val());
+    var number2 = parseInt($("#mul2").val());
+    var result = multiply(number1, number2);
+    $("#result1").text(result);
+  });
+  $("form#division").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+    $("#result2").text(result);
+  });
+});
