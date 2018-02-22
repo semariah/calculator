85c019e058e11c82dfed2cdfcbf9440f14209165
semhar kidane and Thomas Doherty <thomas@>
2018-02-21 18:35:17 -0800

    create new calculator version using if-else

--- js/script.js
+++ js/script.js
@@ -16,33 +16,22 @@ var divide = function(number1, number2) {
 };
 
 //user Logic
-$(document).ready(function(){
-  $("form#add").submit(function(event) {
+$(document).ready(function() {
+  $("form#calculate").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#sub1").val());
-    var number2 = parseInt($("#sub2").val());
-    var result = subtract(number1, number2);
-    $("#result").text(result);
-  });
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#mul1").val());
-    var number2 = parseInt($("#mul2").val());
-    var result = multiply(number1, number2);
-    $("#result1").text(result);
-  });
-  $("form#division").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#div1").val());
-    var number2 = parseInt($("#div2").val());
-    var result = divide(number1, number2);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#result2").text(result);
   });
 });
